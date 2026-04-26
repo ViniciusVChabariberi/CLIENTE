fc86b9069e8ceddb74716cd9a7856de51835cdb4
Vinicius <vini.valero@gmail.com>
2026-04-26 11:24:20 -0300

    alterando configuração das rotas

--- spring/src/main/java/com/vinivictor/api_cliente/controller/ClienteController.java
+++ spring/src/main/java/com/vinivictor/api_cliente/controller/ClienteController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/clientes")
+@RequestMapping("/api/cliente")
 public class ClienteController {
 
     private final ClienteService service;
